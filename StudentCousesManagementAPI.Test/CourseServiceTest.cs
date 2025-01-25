7b27f58d1829f546199be4b666148be6beafd222
Nikolay Miroslavov Kelov <nkelov@gmail.com>
2025-01-25 13:04:01 +0200

    Add Tests

---
+++ StudentCousesManagementAPI.Test/CourseServiceTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentCousesManagementAPI.Data;
+using StudentCousesManagementAPI.Data.Models;
+using StudentCousesManagementAPI.Enums;
+using StudentCousesManagementAPI.Services;
+
+namespace StudentCousesManagementAPI.Test
+{
+    public class CourseServiceTest
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ICourseService _courseService;
+        private readonly IStudentCourseService _studentCourseService;
+
+        public CourseServiceTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                  .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            _context = new ApplicationDbContext(options);
+            _studentCourseService = new StudentCourseService(_context);
+            _courseService = new CourseService(_context, _studentCourseService);
+        }
+        [Fact]
+        public async Task GetCourseById_ShouldWork()
+        {
+            await PopulateDatabase();
+            var course = _courseService.GetCourseById(20);
+            Assert.NotNull(course);
+            Assert.Equal("Foo", course.Name);
+        }
+        [Fact]
+        public async Task GetCourseByName_ShouldWork()
+        {
+            await PopulateDatabase();
+            var course = _courseService.GetCourseByName("Boo");
+            Assert.NotNull(course);
+            Assert.Equal(30, course.Id);
+        }
+        [Fact]
+        public async Task GetCourseProgressStage_ShouldWork()
+        {
+            await PopulateDatabase();
+            var stage = _courseService.GetCourseProgressStage(30);
+
+            Assert.Equal(ProgressStage.InProgress, stage);
+        }
+        [Fact]
+        public async Task GetAllAvailableCourses_ShouldWork()
+        {
+            await PopulateDatabase();
+            var courses = _courseService.GetAllAvailableCourses();
+            Assert.NotNull(courses);
+            Assert.Single(courses);
+        }
+
+        private async Task PopulateDatabase()
+        {
+            var course1 = new Course
+            {
+                Id = 20,
+                Name = "Foo",
+                StartDate = DateOnly.Parse("2025-02-04"),
+                EndDate = DateOnly.Parse("2025-03-04"),
+                Capacity = 10,
+                Stage = Enums.ProgressStage.Created
+            };
+            var course2 = new Course
+            {
+                Id = 30,
+                Name = "Boo",
+                StartDate = DateOnly.Parse("2025-02-04"),
+                EndDate = DateOnly.Parse("2025-03-04"),
+                Capacity = 10,
+                Stage = Enums.ProgressStage.InProgress
+            };
+            var course3 = new Course
+            {
+                Id = 40,
+                Name = "Woo",
+                StartDate = DateOnly.Parse("2025-02-04"),
+                EndDate = DateOnly.Parse("2025-03-04"),
+                Capacity = 10,
+                Stage = Enums.ProgressStage.Finished
+            };
+            await _context.Courses.AddAsync(course1);
+            await _context.Courses.AddAsync(course2);
+            await _context.Courses.AddAsync(course3);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ StudentCousesManagementAPI.Test/StudentServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentCousesManagementAPI.Data;
+using StudentCousesManagementAPI.Data.Models;
+using StudentCousesManagementAPI.DTOs;
+using StudentCousesManagementAPI.Services;
+using Xunit;
+
+namespace StudentCousesManagementAPI.Test
+{
+    public class StudentServiceTests
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IStudentService _studentService;
+        private readonly IStudentCourseService _studentCourseService;
+
+        public StudentServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            _context = new ApplicationDbContext(options);
+            _studentCourseService = new StudentCourseService(_context);
+            _studentService = new StudentService(_context, _studentCourseService);
+        }
+
+        [Fact]
+        public async Task GetStudentById_ShouldWork()
+        {
+            await PopulateDatabase();
+            var student = _studentService.GetStudentById(33);
+            Assert.NotNull(student);
+            Assert.Equal("A", student.Name);
+        }
+        [Fact]
+        public async Task GetStudentByName_ShouldWork()
+        {
+            await PopulateDatabase();
+            var student = _studentService.GetStudentByName("B");
+            Assert.NotNull(student);
+            Assert.Equal(55, student.Id);
+        }
+        [Fact]
+        public async Task CreateStudent_ShouldWork()
+        {
+            await _studentService.CreateStudent("S");
+            var student = _context.Students.Where(s => s.Name == "S").FirstOrDefault();
+
+            Assert.NotNull(student);
+        }
+
+        [Fact]
+        public async Task UpdateStudent_ShouldWork()
+        {
+            await PopulateDatabase();
+            var updated = new StudentDto
+            {
+                Id = 33,
+                Name = "S"
+            };
+            await _studentService.UpdateStudent(updated);
+            var student = _context.Students.Where(s => s.Name == "S").FirstOrDefault();
+
+            Assert.NotNull(student);
+        }
+        [Fact]
+        public async Task DeleteStudent_ShouldWork()
+        {
+            await PopulateDatabase();
+            await _studentService.DeleteStudent(33);
+            var student = _context.Students.Where(s => s.Name == "A").FirstOrDefault();
+            Assert.Null(student);
+        }
+
+        private async Task PopulateDatabase()
+        {
+            var student1 = new Student
+            {
+                Id = 33,
+                Name = "A"
+            };
+            var student2 = new Student
+            {
+                Id = 55,
+                Name = "B"
+            };
+            await _context.Students.AddAsync(student1);
+            await _context.Students.AddAsync(student2);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
