7a1f77e98bab1ecc9a0d3d4fa714352801cb0d31
Nikolay Miroslavov Kelov <nkelov@gmail.com>
2025-01-25 10:43:58 +0200

    Add StudentCourseService

---
+++ StudentCousesManagementAPI/Controllers/StudentsCoursesContoller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentCousesManagementAPI.Services;
+
+namespace StudentCousesManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsCoursesContoller : ControllerBase
+    {
+        private readonly IStudentCourseService _studentCourseService;
+
+        public StudentsCoursesContoller(IStudentCourseService studentCourseService)
+        {
+            _studentCourseService = studentCourseService;
+        }
+        [HttpPost("enroll")]
+        public async Task<IActionResult> EnrollStudentInCourse(int studentId, int courseId)
+        {
+            try
+            {
+                await _studentCourseService.EnrollStudentForCourse(studentId, courseId);
+                return Ok();
+            }
+            catch (NullReferenceException ex)
+            {
+
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpPost("withdraw")]
+        public async Task<IActionResult> WithdrawStudentFromCourse(int studentId, int courseId)
+        {
+            try
+            {
+                await _studentCourseService.WithdrawStudentFromCourse(studentId, courseId);
+                return Ok();
+            }
+            catch (NullReferenceException ex)
+            {
+
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
--- StudentCousesManagementAPI/Services/StudentCourseService.cs
+++ StudentCousesManagementAPI/Services/StudentCourseService.cs
@@ -21,6 +21,8 @@ namespace StudentCousesManagementAPI.Services
 
         public async Task EnrollStudentForCourse(int studentId, int courseId)
         {
+            var sc = _context.StudentCourses.Where(sc => sc.StudentId == studentId && sc.CourseId == courseId).FirstOrDefault();
+            if (sc != null) { throw new NullReferenceException($"Student with ID: {studentId} is already enrolled in Course with Id: {courseId}!"); }
             var student = _context.Students.Where(s => s.Id == studentId).FirstOrDefault();
             if (student == null) { throw new NullReferenceException($"Student with ID: {studentId} does not exist in the Database!"); }
             var course = _context.Courses.Where(c => c.Id == courseId).FirstOrDefault();
@@ -32,6 +34,8 @@ namespace StudentCousesManagementAPI.Services
                 CourseId = courseId
             };
 
+            course.Capacity -= 1;
+            
             await _context.StudentCourses.AddAsync(studentCourse);
             await _context.SaveChangesAsync();
         }
@@ -40,7 +44,10 @@ namespace StudentCousesManagementAPI.Services
         {
            var studentCourse = _context.StudentCourses.Where(sc => sc.StudentId == studentId && sc.CourseId == courseId).FirstOrDefault();
             if (studentCourse == null) { throw new NullReferenceException($"Student with ID: {studentId} is not enrolled in Course with Id: {courseId}!"); }
+            var course = _context.Courses.Where(c => c.Id == courseId).FirstOrDefault();
+            if (course == null) { throw new NullReferenceException($"Course with ID: {courseId} does not exist in the Database!"); }
 
+            course.Capacity += 1;
             _context.StudentCourses.Remove(studentCourse);
             await _context.SaveChangesAsync();
         }
